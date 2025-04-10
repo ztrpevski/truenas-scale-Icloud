845ac2dd329fba0884ee1edbc3a76713b9907885
Zoran <zoran.trpevski@gmail.com>
2025-04-10 19:17:24 +0200

    Update icloudpd.sh

--- icloudpd.sh
+++ icloudpd.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+export PATH=$PATH:/opt/icloudpd/bin/
 
-# Start icloudpd sync (example, add your actual user/params)
-icloudpd \
-    --directory "/home/user/iCloud" 
+# Just to confirm it's working
+echo "icloudpd path: $(which icloudpd)"
+icloudpd --version
 
-# Block so container doesn't exit immediately
-tail -f /dev/null
+# Replace this with your real sync command
+exec icloudpd \
+  --username "$APPLE_ID" \
+  --directory "/icloud" \
+  --cookie-directory "/config" \
+  --recent 100 \
+  --log-level info
