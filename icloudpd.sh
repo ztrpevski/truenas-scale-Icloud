f581a5af12558e58b5052474bb0fa46d43681221
Zoran <zoran.trpevski@gmail.com>
2025-04-10 19:17:50 +0200

    Update icloudpd.sh

--- icloudpd.sh
+++ icloudpd.sh
@@ -7,9 +7,4 @@ echo "icloudpd path: $(which icloudpd)"
 icloudpd --version
 
 # Replace this with your real sync command
-exec icloudpd \
-  --username "$APPLE_ID" \
-  --directory "/icloud" \
-  --cookie-directory "/config" \
-  --recent 100 \
-  --log-level info
+exec icloudpd 
